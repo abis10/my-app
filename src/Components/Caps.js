1e6bc3fd7f6edd7c82aa6418c988c9bfd4ac725c
abis10 <70681025+abis10@users.noreply.github.com>
2020-09-06 21:22:37 -0700

    test

--- src/Components/Caps.js
+++ src/Components/Caps.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-
+import FormList from './Form'
 class Caps extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      capFirst()
+  
+      <FormList>{(this.handleNameChange.charAt(0).toUpperCase().slice(1))}</FormList>
     );
   }
 }
 
-function capFirst(s)
-{
-    return s.charAt(0).toUpperCase() + s.slice(1);
-}
+// function capFirst(s)
+// {
+//     return s.charAt(0).toUpperCase() + s.slice(1);
+// }
 export default Caps;
